06257bb5af7bcd749f12683884b50ec32e6bc2a4
Muniox <muniox20@gmail.com>
2023-09-10 23:54:17 +0200

    feat: Update Application Configuration and Main File
    In this commit, the following changes have been made:
    
    1. Added a new 'config-service.ts' file to manage application configuration using 'nestjs/config' and 'dotenv'. This file exports a 'configService' and 'isProduction' function.
    
    2. Updated 'main.ts' to use the 'isProduction' function from 'config-service.ts' to conditionally enable/disable error messages in the global validation pipe based on the environment.
    
    3. Modified 'migration.config.ts' to import 'configService' from the 'utils.ts' file, ensuring a consistent configuration source for the database setup.
    
    These changes improve configuration management and error handling in the application.
    
    Changes:
    - Added 'config-service.ts'
    - Updated 'main.ts' to use 'isProduction' from 'config-service.ts'
    - Modified 'migration.config.ts' to use 'configService' from 'utils.ts'

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { isProduction } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      disableErrorMessages: false,
+      disableErrorMessages: isProduction(),
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
