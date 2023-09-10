6ae5ac97fdd27599d44c2110c220b8aeade67d74
Muniox <muniox20@gmail.com>
2023-09-10 23:23:10 +0200

    feat(main): Add ValidationPipe and Dependencies
    Enhanced the 'main.ts' file by adding a global 'ValidationPipe' for request payload validation. This pipe uses the 'class-validator' and 'class-transformer' libraries to enforce validation rules on incoming data.
    
    Additionally, updated 'package.json' to include the following dependencies:
    
    - `"class-transformer": "^0.5.1"`
    - `"class-validator": "^0.14.0"`
    
    These changes improve data validation and error handling in the application.
    
    Changes:
    - Updated 'main.ts' to include 'ValidationPipe'
    - Added dependencies in 'package.json'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
