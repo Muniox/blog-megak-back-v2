319366882e67990895dbdd6c1eb24fb4e1e51f8f
Muniox <muniox20@gmail.com>
2023-09-11 12:39:40 +0200

    feat(service): Update User Service Methods
    In this commit, the 'user.service.ts' file has been updated to improve the user service methods. The service now interacts with the database using TypeORM to perform CRUD operations on user entities. Additionally, password hashing is applied before saving the user.
    
    Changes:
    - Updated user service methods to interact with the database using TypeORM
    - Implemented password hashing using 'hash-pwd.ts'

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { hashPwd } from '../utils/hash-pwd';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  // @TODO: Zrób filtr do zwracania użytkownika
+  async create(createUserDto: CreateUserDto) {
+    const user = new User();
+    user.email = createUserDto.email;
+    user.pwdHash = hashPwd(createUserDto.pwd);
+    await user.save();
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return await User.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return await User.findOne({ where: { id } });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await User.findOne({ where: { id } });
+    user.email = updateUserDto.email;
+    user.pwdHash = hashPwd(updateUserDto.pwd);
+    await user.save();
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const user = await User.findOne({ where: { id } });
+    await user.remove();
   }
 }
